22098d80ce494f8a394ab567b3b5b90ed31f2a4b
ichiya <kazuya7187@gmail.com>
2024-04-19 19:41:17 +0900

    update

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
                 fontWeight: "bold",
               }}
             >
-              Share Schedule
+              AttendShare
             </Text>
           </Group>
         </Link>
