209fd37b9dd72d96f20cd211eadef5dbbc65ca86
ichiya <kazuya7187@gmail.com>
2024-04-20 02:46:57 +0900

    implement api route

---
+++ app/api/notes/route.ts
@@ -0,0 +1,23 @@
+import { supabase } from "@/utils/supabase";
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export async function GET(request: any) {
+  const { data, error } = await supabase.from("notes").select();
+  console.log(data);
+
+  if (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,26 +1,17 @@
 "use client";
 
-import { getNotes } from "@/utils/getNotes";
 import { useEffect, useState } from "react";
 
-// import { createClient } from "@/utils/supabase/client";
-// import { supabase } from "@/utils/supabase/supabase";
-
 export default function Notes() {
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  const [data, setData] = useState<any[]>([]);
-
-  const fetchData = async () => {
-    const { data: notes, error } = await getNotes();
-    console.log(notes);
-    console.log(error);
-    if (notes) setData(notes);
-  };
-  // const supabase = createClient();
-  // const { data: notes } = await supabase.from("notes").select();
+  const [data, setData] = useState();
 
   useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("/api/notes");
+      setData(await data.json());
+    };
     fetchData();
-  });
+  }, []);
+
   return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
