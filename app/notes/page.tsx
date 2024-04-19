b91234a2bfc89dcc27fe70bd94064d2336b15f03
ichiya <kazuya7187@gmail.com>
2024-04-20 00:57:05 +0900

    update

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,8 +1,8 @@
-import { createClient } from '@/utils/supabase/server';
+import { createClient } from "@/utils/supabase/client";
 
 export default async function Notes() {
   const supabase = createClient();
   const { data: notes } = await supabase.from("notes").select();
 
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>;
 }
