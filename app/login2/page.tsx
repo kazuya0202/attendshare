b8a039775061db500ac29b93d50273e6be7e1e81
ichiya <kazuya7187@gmail.com>
2024-04-20 02:46:19 +0900

    remove preset of supabase with next.js

--- app/login2/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { LoginForm } from "@/features/login/form";
-
-export default function Login() {
-  return <LoginForm />;
-}
--- utils/getNotes.ts
+++
@@ -1,6 +0,0 @@
-import { supabase } from "@/utils/supabase/supabase";
-
-export const getNotes = async () => {
-  const { data, error } = await supabase.from("notes").select();
-  return { data, error };
-};
