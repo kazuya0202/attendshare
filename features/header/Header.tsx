9e175f44b49141941b9d5efd1741a381666f214d
ichiya <kazuya7187@gmail.com>
2024-07-29 02:28:00 +0900

    supabase auth の処理をリファクタリング

--- features/header/Header.tsx
+++ features/header/Header.tsx
@@ -3,29 +3,19 @@
 import { SignInDialogButton } from "@/components/googleOAuth/SignInDialogButton"
 import { Button } from "@/components/ui/button"
 import { useUserStore } from "@/hooks/useUserStore"
-import { supabase } from "@/utils/supabase"
+import { signOut } from "@/utils/supabase"
 import { IconCalendarTime } from "@tabler/icons-react"
 import Link from "next/link"
 
 export function Header() {
   const { user, updateUser } = useUserStore()
 
-  const signOut = async () => {
-    const { error } = await supabase.auth.signOut()
-    if (error) {
-      console.log(error)
-    } else {
-      supabase.auth.signOut()
-      updateUser(null)
-      console.log("already sign out")
-    }
-  }
-
   return (
     <div>
-      <header className="bg-white text-foreground shadow-sm">
+      <header className="bg-white text-foreground drop-shadow">
         <div className="container flex items-center justify-between h-[60px] mx-auto">
           <Link href="/" className="flex items-center gap-2 font-bold">
+            {/* <Sidebar /> */}
             <IconCalendarTime className="w-10 h-10 bg-primary rounded-sm text-white p-1.5" />
             <span className="ml-2 text-xl">AttendShare</span>
           </Link>
